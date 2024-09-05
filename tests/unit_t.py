2cb070ad0f23b8aac37e0b5c62af02af8350dc0e
uttamtopialifesciences <uttamtopialifesciences@gmail.com>
2024-09-05 19:37:15 +0530

    Update unit_t.py

--- tests/unit_t.py
+++ tests/unit_t.py
@@ -1,11 +1,11 @@
 from src.fun import add,sub
 
 def test_add():
-    assert add(5,6)==11
-    assert add(1,6)==7
-    assert add(-1,6)==5
+    assert add(2,3)==5
+    assert add(-1,1)==0
     
 def test_sub():
-    assert sub(5,6)==-1
-    assert sub(1,6)==-5
-    assert sub(-1,6)==-7
+    assert sub(5,3)==2
+    assert sub(4,3)==1
+    assert sub(3,3)==0
+    assert sub(2,3)==-1
